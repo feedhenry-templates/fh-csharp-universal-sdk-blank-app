9a6621ad2b1669b95e4770324b5f9d0972b30297
Erik Jan de Wit <erikjan.dewit@gmail.com>
2015-08-14 11:26:33 +0200

    added init on both projects

--- fh-vs-csharp-sdk-blank-app/fh-vs-csharp-sdk-blank-app.Windows/MainPage.xaml.cs
+++ fh-vs-csharp-sdk-blank-app/fh-vs-csharp-sdk-blank-app.Windows/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FHSDK;
+using FHSDK81.Phone;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -25,6 +27,37 @@ namespace FHStarterApp
         public MainPage()
         {
             this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+
+            try
+            {
+                bool inited = await FHClient.Init();
+                if (inited)
+                {
+                    //Initialisation is successful
+                }
+            }
+            catch (FHException)
+            {
+                //Initialisation failed, handle exception
+            }
         }
     }
 }
